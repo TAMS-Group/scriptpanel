744a28bbd374d3c3ab92e2547a141bacd2273590
Jonas Tietz <4jtietz@informatik.uni-hamburg.de>
2020-09-03 15:21:55 +0200

    moved to yaml-cpp and cmake

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <vector>
+#include <yaml-cpp/yaml.h>
+#include <fstream>
 
 #include <GL/gl3w.h>
 #include <GLFW/glfw3.h>
@@ -76,180 +78,34 @@ void renderAndSwapBuffers(GLFWwindow *window, ImVec4 clear_color)
 
 struct Button
 {
-    char *label;
-    char *path;
+    std::string label;
+    std::string path;
     bool keep_open;
 };
 
-bool isWhitespace(char c)
-{
-    return (c == ' ' || c == '\n' || c == '\t');
-}
-
+std::string default_label = "NO_LABLE";
+std::string default_path = "/bin/bash";
 std::vector<Button> ParseFile(const char *filename)
 {
     std::vector<Button> result;
-    FILE *file = fopen(filename, "rb");
-    if(file)
+    YAML::Node root = YAML::LoadFile(filename);
+
+    for(int i = 0; i < root.size(); i++)
     {
-	fseek(file, 0, SEEK_END);
-	size_t size = ftell(file);
-	rewind(file);
-	char *buffer = (char *)malloc(size+1);
-	fread(buffer, size, 1, file);
-	buffer[size] = 0;
-	fclose(file);
-
-	char *cur = buffer;
-	bool parsing_button = false;
-	Button button = {};
-	while(*cur)
-	{
-	    char c = *cur;
-	    switch(c)
-	    {
-	    case ' ': case '\n': case '\t': {++cur;}break;
-	    case '{':
-	    {
-		// TODO: error when open multiple curly braces
-		++cur;
-		parsing_button = true;
-		button.label = 0;
-		button.path = 0;
-		button.keep_open = true;
-	    }break;
-	    case '}':
-	    {
-		++cur;
-		parsing_button = false;
-		result.push_back(button);
-	    }break;
-	    default:
-	    {
-		if(strncmp(cur, "Label", 5) == 0)
-		{
-		    cur += 5;
-		    while(isWhitespace(*cur)){++cur;} // TODO: refactor
-		    if(*cur != ':')
-		    {
-			printf("Expected ':' got '%c'. Skipping line.\n", *cur);
-			while(*cur != '\n') {++cur;};
-		    }
-		    ++cur;
-		    while(isWhitespace(*cur)){++cur;}
-		    
-		    char *start = cur;
-		    while(*cur != '\n'){++cur;}
-		    size_t length = cur - start;
-		    button.label = (char *)malloc(length + 1);
-		    memcpy(button.label, start, length);
-		    button.label[length] = 0;
-		}
-		else if(strncmp(cur, "Path", 4) == 0)
-		{
-		    cur += 4;
-		    while(isWhitespace(*cur)){++cur;} // TODO: refactor
-		    if(*cur != ':')
-		    {
-			printf("Expected ':' got '%c'. Skipping line.\n", *cur);
-			while(*cur != '\n') {++cur;};
-		    }
-		    ++cur;
-		    while(isWhitespace(*cur)){++cur;}
-
-		    //const char *extra = "; exec bash";
-		    char *start = cur;
-		    while(*cur != '\n'){++cur;}
-		    size_t length = cur - start;
-		    button.path = (char *)malloc(length); // + 13);
-		    memcpy(button.path, start, length);
-		    //memcpy(button.path+length, extra, 12);
-		    button.path[length] = 0; // +12] = 0; // TODO: this is not needed
-		}
-		else if(strncmp(cur, "KeepOpen", 8) == 0)
-		{
-		    cur += 8;
-		    while(isWhitespace(*cur)){++cur;} // TODO: refactor
-		    if(*cur != ':')
-		    {
-			printf("Expected ':' got '%c'. Skipping line.\n", *cur);
-			while(*cur != '\n') {++cur;};
-		    }
-		    ++cur;
-		    while(isWhitespace(*cur)){++cur;}
-
-		    if(strncmp(cur, "true", 4) == 0)
-		    {
-			cur+=4;
-			button.keep_open = true;
-		    }
-		    else if(strncmp(cur, "false", 5) == 0)
-		    {
-			cur+=5;
-			button.keep_open = false;
-		    }
-		    else
-		    {
-			char *start = cur;
-			while(*cur != '\n') {++cur;};
-			printf("Expected 'true' or 'false' got '%.*s'. Skipping line.\n", (int)(cur-start), start);		
-		    }
-		}
-		else
-		{
-		    printf("unexpected char '%c'\n", c);
-		    ++cur;
-		}
-	    }
-	    }
-	}	
-	free(buffer);
+        Button button = {default_label, default_path, false};
+        if(root[i]["Label"])
+            button.label = root[i]["Label"].as<std::string>();
+        if(root[i]["Path"])
+            button.path = root[i]["Path"].as<std::string>();
+        if(root[i]["KeepOpen"])
+            button.keep_open = root[i]["KeepOpen"].as<bool>();
+        result.push_back(button);
     }
-    return result;
-}
-
-void appenToFile(const char *filename, Button button)
-{
-    FILE *file = fopen(filename, "ab");
-    if(file)
-    {
-	fputs("\n{\n", file);
-
-	fputs("\tLabel: ", file);
-	fputs(button.label, file);
-	fputc('\n', file);
-	fputs("\tPath: ", file);
-	fputs(button.path, file);
-	fputc('\n', file);
-	if(!button.keep_open)
-	    fputs("\tKeepOpen: false\n", file);
-	
-	fputs("}\n", file);
-	fclose(file);
-    }    
-}
-
-Button copyPersistentButton(Button button)
-{
-    Button result;
-
-    const char *extra = "; exec bash";
     
-    size_t label_len = strlen(button.label) + 1;
-    result.label = (char *)malloc(label_len);
-    memcpy(result.label, button.label, label_len);
-
-    size_t path_len = strlen(button.path);
-    result.path = (char *)malloc(path_len +1); // + 13);
-    memcpy(result.path, button.path, path_len);
-    result.path[path_len] = 0;
-    //memcpy(result.path+path_len, extra, 13);		    
-
-    result.keep_open = button.keep_open;
     return result;
 }
 
-#define CONFIG_FILE "control_panel.txt"
+#define CONFIG_FILE "../config/script_panel.yaml"
 int main(int, char**)
 {
     std::vector<Button> buttons = ParseFile(CONFIG_FILE);
@@ -258,10 +114,6 @@ int main(int, char**)
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     ImGuiStyle& style = ImGui::GetStyle();
 
-    // Note: buffers for the add button popup dialog
-    char add_button_label[32] = "";
-    char add_button_path[64] = "";
-    bool add_button_keep_open = true;
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
@@ -270,88 +122,50 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-	{
-	    bool open_add_button_popup = false;
-	    if(ImGui::BeginMainMenuBar())
-	    {
-		if(ImGui::BeginMenu("Edit"))
-		{
-		    if(ImGui::MenuItem("Add Button"))
-		    {
-			open_add_button_popup = true;
-		    }
-		    ImGui::EndMenu();
-		}
-		ImGui::EndMainMenuBar();
-	    }
-	    if(open_add_button_popup)
-		ImGui::OpenPopup("add_button");
-	    if(ImGui::BeginPopup("add_button"))
-	    {
-		ImGui::InputText("Label", add_button_label, 32);
-		ImGui::InputText("Path", add_button_path, 64);
-		ImGui::Checkbox("KeepOpen", &add_button_keep_open);
-		
-		if(ImGui::Button("Add"))
-		{
-		    Button button = {add_button_label, add_button_path, add_button_keep_open};
-		    appenToFile(CONFIG_FILE, button);
-		    buttons.push_back(copyPersistentButton(button)); 
-		    open_add_button_popup = false;
-		}
-		ImGui::EndPopup();
-	    }
-	}
+        {
+            ImGuiViewport *viewport = ImGui::GetWindowViewport();
+            ImGuiID id = ImGui::DockSpaceOverViewport(viewport);
+            ImGui::SetNextWindowDockID(id);
 
-	{
-	    ImGuiViewport *viewport = ImGui::GetWindowViewport();
-	    ImGuiID id = ImGui::DockSpaceOverViewport(viewport);
-	    ImGui::SetNextWindowDockID(id);
+            if(ImGui::Begin("Controlpanel"))
+            {
+                float window_max_x = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
+                int button_id = 0;
+                for(auto &button : buttons)
+                {
+                    ImGui::PushID(button_id);
+                    if(ImGui::Button(button.label.c_str(), {125,125}))
+                    {
+                        pid_t pid = fork();
+                        if(pid)
+                        {
+                            if(button.keep_open)
+                            {
+                                std::string extra = "; exec bash";
+                                std::string cmd = button.path + extra;
+                                execl("/usr/bin/gnome-terminal", "ControlpanelTerminal", "--" , "bash", "-c", cmd.c_str(), 0);
+                            }
+                            {
+                                execl("/usr/bin/gnome-terminal", "ControlpanelTerminal", "--" , "bash", "-c", button.path.c_str(), 0);
+                            }
+                        }
+                    }
+		    
+                    float current_button_max_x = ImGui::GetItemRectMax().x;
+                    float next_button_max_x    = current_button_max_x + style.ItemSpacing.x + 100;
+		    
+                    ++button_id;
+                    if (button_id < buttons.size() && next_button_max_x < window_max_x)
+                        ImGui::SameLine();
+		    
+                    ImGui::PopID();
+                }
+                ImGui::End();
+            }
 
-	    if(ImGui::Begin("Controlpanel"))
-	    {
-		float window_max_x = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
-		int button_id = 0;
-		for(auto &button : buttons)
-		{
-		    ImGui::PushID(button_id);
-		    if(ImGui::Button(button.label, {100,100}))
-		    {
-			pid_t pid = fork();
-			if(pid)
-			{
-			    if(button.keep_open)
-			    {
-				    const char *extra = "; exec bash";
-    
-				    size_t path_len = strlen(button.path);
-				    char *cmd = (char *)malloc(path_len + 13);
-				    memcpy(cmd, button.path, path_len);
-				    memcpy(cmd+path_len, extra, 13);		    
-				    execl("/usr/bin/gnome-terminal", "ControlpanelTerminal", "--" , "bash", "-c", cmd, 0);
-				    free(cmd);
-			    }
-			    {
-				execl("/usr/bin/gnome-terminal", "ControlpanelTerminal", "--" , "bash", "-c", button.path, 0);
-			    }
-			}
-		    }
-		    
-		    float current_button_max_x = ImGui::GetItemRectMax().x;
-		    float next_button_max_x    = current_button_max_x + style.ItemSpacing.x + 100;
-		    
-		    ++button_id;
-		    if (button_id < buttons.size() && next_button_max_x < window_max_x)
-			ImGui::SameLine();
-		    
-		    ImGui::PopID();
-		}
-		ImGui::End();
-	    }
-
-	}
+        }
 	
-	renderAndSwapBuffers(window, clear_color);
+        renderAndSwapBuffers(window, clear_color);
     }
 
     cleanupGlfwAndImgui(window);
