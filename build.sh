050a6c5f2b1c7e302d24aa50cd6b3bbd498a6384
Hongzhuo Liang <lianghongzhuo@126.com>
2021-07-20 00:30:33 +0200

    update script pannel to make system find the icon

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ git submodule init
 git submodule update
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/bin
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/.local
 make
 make install
 cd ..
-sed -e "s,Icon=.*,Icon=$PWD/scriptpanel.svg,g" script_panel.desktop-template > script_panel.desktop
-chmod +x script_panel.desktop
-cp script_panel.desktop $HOME/Desktop
+cp script_panel.desktop.in $HOME/.local/share/applications/script_panel.desktop
+cp scriptpanel.png $HOME/.local/share/icons/hicolor/128x128/apps/scriptpanel.png
+cp scriptpanel.svg $HOME/.local/share/icons/hicolor/scalable/scriptpanel.svg
